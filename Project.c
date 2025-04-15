11392327e41ffbaca3c5a682981bd0226b978525
Jana Fahmy <janafahmy1@gmail.com>
2025-04-15 04:26:27 +0200

    main project file

---
+++ Project.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include <stdint.h>
+
+
+// Implement the memory: 2048 * 32-bit word-addressable memory 
+// Create register structure (R0-R31, PC)
+// Initialize memory regions (Instructions: 0 - 1023, Data: 1024-2047)
+
+
+#define MemorySize 2048
+#define InstructionLimit 1024 // 0  to 1023
+#define DataStart 1024
+#define RegisterNo 32
+
+uint32_t memory[MemorySize]; // Unsigned 32-bit integer
+uint32_t registers[RegisterNo];
+uint32_t PC = 0;
+
+
+//Instruction Types 
+
+
+//Instruction Structure 
+
+
+//Array to store the instructions
+
+//Data Structure
+
+
+//Initialization Functions
+    void initialize_memory() {
+        for(int i = 0; i < MemorySize; i++){
+            memory[i] = 0;
+        }
+    }
+
+    void initialize_registers(){
+        for (int i = 0; i < RegisterNo; i++) {
+            registers[i] = 0;
+        }
+        PC = 0;
+    }
+
+//Printing Functions
+    void print_registers(){
+        for(int i = 0; i < RegisterNo; i++){
+            printf("R%-2d: %08X\n", i, registers[i]); //prints the value of each register in hexadecimal digits
+        }
+        printf("PC: %08X\n", PC); //current value of PC
+    }
+
+    void print_memory(int from, int to){
+        for(int i = from; i <= to && i < MemorySize; i++){
+            printf("MEM[%4d]: %08X\n", i, memory[i]);
+        }
+
+    }
+
+//Functions for Instruction Parsing
+
+//Pipeline logic
+
+//Main loop (pipeline)
+
+//Instruction Execution Logic
+
+//Execution Functions
+
+//WB stage
+
+//MEM stage
+
+//Final State (for both registvers & memory)
+
+//Testing
+    void test_initialization(){
+        printf("\n>>> Running Milestone 1 Test\n");
+        print_registers();
+        print_memory(0, 10);                      // Instructions region
+        print_memory(DataStart, DataStart + 6); // Start of data segment
+    }
+
+//Main method
+    int main(){
+        initialize_registers();
+        initialize_memory();
+        test_initialization();
+        return 0;
+    }
+
